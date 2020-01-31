4a78a5c0df6618fe96d978a8028f10708e451fe9
Rasel Ahmmed Bappi <raselahmmed@hotmail.com>
2020-01-31 16:16:43 +0600

    2020-01-31

---
+++ RnD.WebCoreApp/RnD.WebCoreDbSeedApp/RnD.WebCoreDbSeedApp/Models/AppConstant.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RnD.WebCoreDbSeedApp.Models
+{
+    public static class AppConstant
+    {
+        public static class SuperAdminUser
+        {
+            public static string UserName = "SuperAdmin";
+            public static string Email = "superadmin@mail.com";
+            public static string UserPassword = "Qwer!234";
+            public static string RoleName = "SuperAdmin";
+        }
+
+        public static class AdminUser
+        {
+            public static string UserName = "Admin";
+            public static string Email = "admin@mail.com";
+            public static string UserPassword = "Qwer!234";
+            public static string RoleName = "Admin";
+        }
+
+        public static class ManagerUser
+        {
+            public static string UserName = "Manager";
+            public static string Email = "manager@mail.com";
+            public static string UserPassword = "Qwer!234";
+            public static string RoleName = "Manager";
+        }
+
+        public static class MemberUser
+        {
+            public static string UserName = "Member";
+            public static string Email = "member@mail.com";
+            public static string UserPassword = "Qwer!234";
+            public static string RoleName = "Member";
+        }
+    }
+}
---
+++ RnD.WebCoreApp/RnD.WebCoreDbSeedApp/RnD.WebCoreDbSeedApp/ViewModels/IdentityUserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RnD.WebCoreDbSeedApp.ViewModels
+{
+    public class IdentityUserViewModel
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string UserPassword { get; set; }
+        public string RoleName { get; set; }
+    }
+}
