474d1f4cba3acc3e47b0aa8e0419964a7cc4f1eb
Rasel Ahmmed Bappi <raselahmmed@hotmail.com>
2021-07-06 17:17:05 +0600

    Added CI/CD Pipeline Application

---
+++ RnD.CICDPipeline/RnD.CICDPipeline/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RnD.CICDPipeline.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
